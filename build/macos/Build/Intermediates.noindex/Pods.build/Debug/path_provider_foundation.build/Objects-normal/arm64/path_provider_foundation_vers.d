dce97530c6529740ef11a3f5f576a45670262567
Valentin <luginbuhlvalentin@gmail.com>
2025-02-20 11:36:25 +0100

    update

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/Objects-normal/arm64/path_provider_foundation_vers.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/lugin/Projects/GitHub/minesweeper-app/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/module.modulemap \
+  /Users/lugin/Projects/GitHub/minesweeper-app/build/macos/Build/Intermediates.noindex/Pods.build/Debug/path_provider_foundation.build/DerivedSources/path_provider_foundation_vers.c
