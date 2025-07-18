b62a8fed90c6246e20ca8725bc7980801a413014
Valentin <luginbuhlvalentin@gmail.com>
2025-07-18 09:15:31 +0200

    update

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/Objects-normal/arm64/shared_preferences_foundation_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/lugin/Projects/GitHub/minesweeper-app/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/module.modulemap \
-  /Users/lugin/Projects/GitHub/minesweeper-app/build/macos/Build/Intermediates.noindex/Pods.build/Debug/shared_preferences_foundation.build/DerivedSources/shared_preferences_foundation_vers.c
